9082573b16bad9a3553de6173196602560a77708
Evan Jones <evan.jones@datadoghq.com>
2020-07-09 13:22:26 -0400

    combinestacks: Support file uploads

--- combinestacks_test.go
+++ combinestacks_test.go
@@ -1,6 +1,11 @@
 package main
 
 import (
+	"bytes"
+	"io/ioutil"
+	"mime/multipart"
+	"net/http"
+	"net/http/httptest"
 	"reflect"
 	"strings"
 	"testing"
@@ -41,6 +46,88 @@ func TestParse(t *testing.T) {
 			t.Error("ACTUAL:")
 			t.Errorf("%#v", output)
 		}
+	}
+}
+
+func TestEmptyUpload(t *testing.T) {
+	s := httptest.NewServer(makeHandlers())
+	defer s.Close()
+
+	const boundary = "QQQQQboundary"
+	const emptyBody = "--" + boundary + "--"
+	req, err := http.NewRequest(http.MethodPost, s.URL+uploadPath, strings.NewReader(emptyBody))
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("Content-Type", "multipart/form-data; boundary="+boundary)
+	resp, err := s.Client().Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusBadRequest {
+		t.Fatal("unexpected status", resp.Status)
+	}
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = resp.Body.Close()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !bytes.Contains(bodyBytes, []byte("must provide content")) {
+		t.Error("unexpected body:", string(bodyBytes))
+	}
+}
+
+func TestFileUpload(t *testing.T) {
+	s := httptest.NewServer(makeHandlers())
+	defer s.Close()
+
+	// create the multipart request
+	reqBuf := &bytes.Buffer{}
+	reqWriter := multipart.NewWriter(reqBuf)
+	w, err := reqWriter.CreateFormFile(fileFormID, "example.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = w.Write([]byte(plain))
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = reqWriter.Close()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	req, err := http.NewRequest(http.MethodPost, s.URL+uploadPath, reqBuf)
+	req.Header.Set("Content-Type", reqWriter.FormDataContentType())
+	if err != nil {
+		t.Fatal(err)
+	}
+	resp, err := s.Client().Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		t.Fatal("unexpected status", resp.Status)
+	}
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = resp.Body.Close()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !bytes.Contains(bodyBytes, []byte("state=[running]")) {
+		t.Error("unexpected body:", string(bodyBytes))
 	}
 }
 
